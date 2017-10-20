621e4ce514f0a98b835a21ced1de0e3095dc0c6e
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-10-20 13:11:45 -0400

    Add Procfile

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'mysql://cojj0tgonzu3p4tn:a9luveb3z784fhkf@thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vhxlrkzu6s207zn6'),
+            'username' => env('DB_USERNAME', 'cojj0tgonzu3p4tn'),
+            'password' => env('DB_PASSWORD', 'a9luveb3z784fhkf'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
