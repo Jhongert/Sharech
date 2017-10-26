d561f12122e50d68cc240c816da478524e0cde7c
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-10-25 23:33:16 -0400

    add heroku db settings

--- config/database.php
+++ config/database.php
@@ -2,6 +2,11 @@
 
 return [
 
+    $url = parse_url(getenv("JAWSDB_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -41,11 +46,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'appdb'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
