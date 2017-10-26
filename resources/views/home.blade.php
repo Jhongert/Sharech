93e371f23c290a2fec753d775cd6cf041a1d5a57
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-10-26 16:11:01 -0400

    Fix home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,9 @@
             <div class="col-md-6">
            
             	<div class="row">
+                    <div style="margin: 20px 0">
+                        <h1 class="text-center">A better place for developers</h1>
+                    </div>
                     <div class="col-xs-4">
             		    <h1 class="text-center"><i class="fa fa-share-alt fa-3x"></i></h1>
                         <h1 class="text-center">Share</h1>
@@ -25,13 +28,6 @@
                         <h1 class="text-center">Connect</h1>
             		</div>
             	</div>
-                
-                <div style="margin: 20px 0">
-                    <p >is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    </p>
-                    <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    </p>
-                </div>
             </div>
             <div class="col-md-6">
                 @include('auth.signupform')
